d3efdb417eba7dd686e70208b42b16bcc43fbeea
Iyana Marquez <yayakix@gmail.com>
2022-05-21 18:02:35 -0400

    working on fixing replay, resetting gameover

--- script.js
+++ script.js
@@ -6,10 +6,15 @@ let testPattern = [...gamePattern];
 let colors = ["purple", "pink", "blue", "green"];
 
 $(document).keypress(function () {
+  console.log("a key has been pressed")
   if (gameOn === false) {
+
     // $("h1").text("Levell: " + level);
     gameOn = true;
+    console.log("the game is now true")
 
+  gamePattern = [];
+  userPattern = [];
     gameSequence();
 
     $(".box").click(function (e) {
@@ -18,7 +23,15 @@ $(document).keypress(function () {
       //  push user pattern to array
 
       buttonPress(clickedColor);
+        console.log("-------After color click-------");
+        console.log("game pattern: " + gamePattern);
+        console.log("user pattern: " + userPattern);
+        console.log("user length is" + userPattern.length);
+        console.log("level is " + level);
 
+        setTimeout(function () {
+          console.log("After 5 seconds!");
+        }, 2000);
       checkAns2();
     });
     //
@@ -26,10 +39,14 @@ $(document).keypress(function () {
 });
 
 function gameSequence() {
+  
   level++;
   $("h1").html("Level: " + level);
   let nextColor = colors[Math.floor(Math.random() * 4)];
   gamePattern.push(nextColor);
+  console.log(
+    "level: " + level + " user: " + userPattern + " game: " + gamePattern
+  );
   makeFlash(nextColor);
 }
 
@@ -60,46 +77,50 @@ function gameOver() {
   }, 300);
 
   $("h1").text("Press Any Key to Start");
-  console.log("game over");
+  console.log("-----game over---");
 
-  
+   console.log("------gameoversection--------");
+   console.log("game pattern: " + gamePattern);
+   console.log("user pattern: " + userPattern);
+   console.log("user length is" + userPattern.length);
+   console.log("level is " + level);
 }
 
-function checkAns() {
-  console.log("--------------");
-  console.log("game pattern: " + gamePattern);
-  console.log("user pattern: " + userPattern);
-  console.log("user length is" + userPattern.length);
-  console.log("level is " + level);
+// function checkAns() {
+//   console.log("--------------");
+//   console.log("game pattern: " + gamePattern);
+//   console.log("user pattern: " + userPattern);
+//   console.log("user length is" + userPattern.length);
+//   console.log("level is " + level);
 
-  if (userPattern.length == level) {
-    let testArea = [];
-    console.log("checking answer");
-    for (let i = 0; i < userPattern.length; i++) {
-      if (userPattern[i] === gamePattern[i]) {
-        testArea.push(true);
-        //  how to end loop if it catches an error
+//   if (userPattern.length == level) {
+//     let testArea = [];
+//     console.log("checking answer");
+//     for (let i = 0; i < userPattern.length; i++) {
+//       if (userPattern[i] === gamePattern[i]) {
+//         testArea.push(true);
+//         //  how to end loop if it catches an error
 
-        console.log("correct?");
-      } else {
-        console.log("wrong?");
-        testArea.push(false);
-      }
-    }
-    if (testArea.includes(false)) {
-      console.log("somethign wrong was found");
-      gameOver();
-    } else {
-      console.log("running next round");
-      userPattern = [];
-      gameSequence();
-    }
-  } else {
-    console.log("waiting for more clicks");
-  }
-  // gameSequence()
-  //  testPattern.shift();
-}
+//         console.log("correct?");
+//       } else {
+//         console.log("wrong?");
+//         testArea.push(false);
+//       }
+//     }
+//     if (testArea.includes(false)) {
+//       console.log("somethign wrong was found");
+//       gameOver();
+//     } else {
+//       console.log("running next round");
+//       userPattern = [];
+//       gameSequence();
+//     }
+//   } else {
+//     console.log("waiting for more clicks");
+//   }
+//   // gameSequence()
+//   //  testPattern.shift();
+// }
 
 //  make a check answer function after each click and check every user pattern until it matches game pattern length
 function checkAns2() {
@@ -109,9 +130,9 @@ function checkAns2() {
     for (let i = 0; i < userPattern.length; i++) {
       if (userPattern[i] !== gamePattern[i]) {
         wrong += 1;
-        console.log("wrong answer1");
+        console.log("wrong answer while still below gp");
       } else {
-        console.log("not wrong ans1");
+        console.log("not wrong ans while below gp");
         // i want to keep the game going, aka wait for more choices
       }
     }
@@ -119,29 +140,63 @@ function checkAns2() {
       console.log("restart game");
       gameOver();
     } else {
-      console.log("continue game");
+      console.log("continue listening for clicks");
     }
+      if (wrong > 0) {
+        gameOver();
+        console.log("restart game");
+      } else {
+        console.log("continue game pattern");
+        userPattern = [];
+        gameSequence();
+      }
+
+
     //
   } else if (userPattern.length === gamePattern.length) {
     // else if user pattern is equal to game pattern, this will be the check that causes the round to move on
     // empty user pattern for them to try again'
     for (let i = 0; i < userPattern.length; i++) {
-      if (userPattern[i] !== gamePattern[i]) {
+      if (userPattern[length - 1] !== gamePattern[length - 1]) {
+        console.log("user lenth: " + userPattern[length - 1]);
         wrong += 1;
-        console.log("wrong answer2");
+        // just testing last position instead of looping
+        console.log("wrong answer chosen on final check");
+
       } else {
-        console.log("not wrong ans2");
+        // length is undefined
+        console.log(
+          "user lenth: " +
+            userPattern[userPattern.length - 1] +
+            "gam length: " +
+            gamePattern[userPattern.length - 1]
+        );
+        // userPattern = [];
+        console.log("not wrong ans at final check");
         // i want to keep the game going, aka wait for more choices
       }
     }
-    if (wrong > 0) {
-      gameOver();
-      console.log("restart game");
-    } else {
-      console.log("continue game");
-      userPattern = [];
-      gameSequence();
-    }
-  }
-}
+      if (wrong > 0) {
+        gameOver();
+        console.log("restart game");
+      } else {
+        console.log("continue game pattern");
+        userPattern = [];
+        gameSequence();
+      }
 
+
+  
+    }
+      // if (wrong > 0) {
+      //   gameOver();
+      //   console.log("restart game");
+      // } else {
+      //   console.log("continue game pattern");
+      //   userPattern = [];
+      //   gameSequence();
+      // }
+
+
+  }
+  
